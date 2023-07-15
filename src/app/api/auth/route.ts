d6bcdb396624450ab5ed7207576660341eb7cba0
Abner Santos <abner.kif1@gmail.com>
2023-07-15 17:42:32 -0300

    error request

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -11,6 +11,7 @@ export async function POST(request: NextRequest){
   const { email, password } = await authenticateBodySchema.parse(body);
   const { origin } = new URL(request.url);
   const prisma = new PrismaClient();
+  console.log((await prisma.user.findMany()).length)
 
   const user = await prisma.user.findFirst({ where: { email, password } });
   if(!user){
