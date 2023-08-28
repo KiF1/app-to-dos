8e4333d18f146121f2ec83eadc9e7b4d0b02afce
unknown <Administrador@UNI001_L003_011.serlab.intra>
2023-08-28 20:34:35 -0300

    remove route logout

--- src/components/Header/HeaderBig.tsx
+++ src/components/Header/HeaderBig.tsx
@@ -2,21 +2,28 @@
 
 import Link from "next/link";
 import { LayoutDashboard, LogOut } from "lucide-react";
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
+import Cookies from "js-cookie";
 
 
 export function HeaderBig(){
   const currentRoute = usePathname();
   const dashboardRouteActive: boolean = currentRoute === '/dashboard';
+  const router = useRouter();
+
+  function signOut(){
+    Cookies.remove('token_role');
+    router.push('/')
+  }
 
   return(
     <div className="w-full mt-auto h-fit bg-gray-200 shadow-lg flex gap-4 justify-around items-center p-4">
        <Link href="/dashboard" data-active={dashboardRouteActive} className="w-fit flex items-center py-2 px-4 font-bold data-[active=true]:border-t-2 data-[active=true]:border-black">
         {dashboardRouteActive ? <LayoutDashboard color="black" /> : <LayoutDashboard color="gray" />  }
       </Link>
-      <Link href="/api/logout" className="w-fit flex items-center py-2 px-4  gap-4 text-white ">
+      <button onClick={() => signOut()}  className="w-fit flex items-center py-2 px-4  gap-4 text-white ">
         <LogOut color="gray" />
-      </Link>
+      </button>
     </div>
   )
 }
