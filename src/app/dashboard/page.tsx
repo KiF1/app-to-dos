bc98efe0cfef7cabe378613897f5695ce7887af2
Abner Santos <abner.kif1@gmail.com>
2023-08-28 14:27:43 -0300

    add loading

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,6 +6,7 @@ import { Check, X } from "lucide-react";
 import { api } from "../lib/api";
 import Cookies from "js-cookie";
 import { useQuery } from "@tanstack/react-query";
+import ReactLoading from "react-loading";
 
 export interface Todo{
   id: number;
@@ -44,19 +45,25 @@ export default function Dashboard(){
       </div>
       <div className="h-[85vh] flex relative overflow-y-scroll">
         <div className="w-full absolute flex flex-col gap-12 p-4 lg:pr-8 pb-12">
-          <div className="w-full flex flex-col gap-4">
-            {data !== undefined && data.map(item => (
-              <div key={item.id} className="w-full shadow-sm bg-white p-8 flex flex-col justify-center rounded-md items-center gap-2">
-                <strong className="text-lg font-bold">Id: {item.product_id}</strong>
-                <span className="text-lg font-bold">Código: {item.cod}</span>
-                <span className="text-lg font-bold">R$ {item.price}</span>
-                <div className="w-full grid grid-cols-2 gap-4 mt-4">
-                  <button onClick={() => setRed(item.id)} type="button" className="w-full border-0 rounded-lg text-center bg-red p-4"><X className="m-auto" color="white" /></button>
-                  <button onClick={() => setGreen(item.id)} type="button" className="w-full border-0 rounded-lg text-center bg-green-600 p-4"><Check className="m-auto" color="white" /></button>
+          {data !== undefined ? (
+            <div className="w-full flex flex-col gap-4">
+              {data.map(item => (
+                <div key={item.id} className="w-full shadow-sm bg-white p-8 flex flex-col justify-center rounded-md items-center gap-2">
+                  <strong className="text-lg font-bold">Id: {item.product_id}</strong>
+                  <span className="text-lg font-bold">Código: {item.cod}</span>
+                  <span className="text-lg font-bold">R$ {item.price}</span>
+                  <div className="w-full grid grid-cols-2 gap-4 mt-4">
+                    <button onClick={() => setRed(item.id)} type="button" className="w-full border-0 rounded-lg text-center bg-red p-4"><X className="m-auto" color="white" /></button>
+                    <button onClick={() => setGreen(item.id)} type="button" className="w-full border-0 rounded-lg text-center bg-green-600 p-4"><Check className="m-auto" color="white" /></button>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
+          ) : (
+            <div className="w-full h-[70vh] flex items-center justify-center">
+              <ReactLoading className="w-fit" type="spinningBubbles" color="#000000" height='80px' width='100px' />
+            </div>
+          )}
         </div>
       </div>
       <HeaderBig />
